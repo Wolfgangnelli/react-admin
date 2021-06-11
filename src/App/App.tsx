32ca93c2fe13c978c923f3a2f344ca9857626169
[AndreaS] <solfa_90@hotmail.it>
2021-06-11 13:43:34 +0200

    changes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
-import logo from '../logo.svg';
 import './index.css';
 
 function App() {
