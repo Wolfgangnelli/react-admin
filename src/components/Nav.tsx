fdfeeeecde822ce2ab4dcbfa1bf3d0a7d0e495c5
[AndreaS] <solfa_90@hotmail.it>
2021-06-15 14:08:20 +0200

    orders for each ambassador

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,16 +5,21 @@ import {NavLink} from 'react-router-dom';
 const Nav = () => {
     return (
         <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-        <div className="position-sticky pt-3">
+        <div className="position-sticky pt-4">
           <ul className="nav flex-column">
-            <li className="nav-item">
+            <li className="nav-item py-1">
               <NavLink to="/users" activeClassName="selected">
                 Users
               </NavLink>
             </li>
-            <li className="nav-item">
+            <li className="nav-item py-1">
               <NavLink to="/products" activeClassName="selected">
                 Products
+              </NavLink>
+            </li>
+            <li className="nav-item py-1">
+              <NavLink to="/orders" activeClassName="selected">
+                Orders
               </NavLink>
             </li>
           </ul>
