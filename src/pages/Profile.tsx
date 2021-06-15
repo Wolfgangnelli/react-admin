f2a3b9ef311d5cf5149efeec12f3cb1358102647
[AndreaS] <solfa_90@hotmail.it>
2021-06-15 16:27:26 +0200

    add profile

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,86 @@
+import React, {useState, useEffect, SyntheticEvent} from 'react';
+import {Button} from '@material-ui/core';
+import Layout from '../layout/layout';
+import axios from 'axios';
+import {API_ADMIN} from '../config/config';
+
+
+const Profile = () => {
+    const [first_name, setFirst_name] = useState("");
+    const [last_name, setLast_name] = useState("");
+    const [email, setEmail] = useState("");
+    const [passsword, setPasssword] = useState("");
+    const [password_confirmation, setPassword_confirmation] = useState("");
+  
+
+    useEffect(() => {
+        (
+            async () => {
+                const {data} = await axios.get(`${API_ADMIN}user`);
+                   
+                setFirst_name(data.first_name);
+                setLast_name(data.last_name);
+                setEmail(data.email);
+            }
+        )();
+    }, [])
+
+
+    const handleSubmit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        if(e.currentTarget.getAttribute('id')) {           
+                    await axios.put(`${API_ADMIN}users/info`, {
+                        first_name,
+                        last_name,
+                        email
+                    });         
+        } else {           
+                    await axios.put(`${API_ADMIN}users/password`, {
+                        passsword,
+                        password_confirmation
+                    })          
+            }
+    }
+    
+    return (
+        <Layout>
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 mb-10">
+            <div className="py-4">
+                <h3 className="text-2xl font-bold text-green-600">Account Information</h3>
+                <form className="pt-6" onSubmit={handleSubmit} id="account-info">
+                    <div className="form-floating mb-3">
+                        <input type="text" className="form-control" value={first_name} id="first_name" name="first_name" onChange={(e) => setFirst_name(e.target.value)} />
+                        <label htmlFor="first_name">First Name</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="last_name" name="last_name" value={last_name} onChange={(e) => setLast_name(e.target.value)} />
+                        <label htmlFor="last_name">Last Name</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="email" name="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <label htmlFor="email">Email</label>
+                    </div>
+                    <Button variant="contained" color="primary" type="submit">Submit</Button>
+                </form>
+            </div>
+            <div className="py-4">
+            <h3 className="text-2xl font-bold text-green-600">Change Password</h3>
+                <form className="pt-6" onSubmit={handleSubmit}>
+                    <div className="form-floating mb-3">
+                        <input type="password" className="form-control" id="password" name="password" onChange={(e) => setPasssword(e.target.value)} />
+                        <label htmlFor="password">Password</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input type="password" className="form-control" id="password_confirmation" name="password_confirmation" onChange={(e) => setPassword_confirmation(e.target.value)} />
+                        <label htmlFor="password_confirmation">Password Confirm</label>
+                    </div>
+                    <Button variant="contained" color="primary" type="submit">Submit</Button>
+                </form>
+            </div>
+            </main>
+        </Layout>
+    )
+}
+
+export default Profile;
